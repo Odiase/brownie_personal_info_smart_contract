808caf2afb698ceec80ae995466bd4a5a2e229d8
Odiase <odiaseefosa18@gmail.com>
2022-12-13 19:07:19 +0100

    removed print statements

--- scripts/interactions.py
+++ scripts/interactions.py
@@ -8,9 +8,6 @@ from brownie import PersonalInfo, accounts, config
 def read_contract():
     '''Interacting with a smart contract using the abi and the address'''
     personal_info = PersonalInfo[-1]
-    # personal_info.change_favourite_number(15, {"from" : config['wallets']['address']})
-    
-    print(personal_info.view_favourite_number())
 
 
 def main():
